f522f9ed37653d463fa0a0f4dae1560b2e3fd4f7
hrjo0120 <hrjo0120@gmail.com>
2024-11-04 14:36:33 +0900

    todo CRUD 구현

--- src/main/java/com/koreait/Main.java
+++ src/main/java/com/koreait/Main.java
@@ -1,17 +1,45 @@
 package com.koreait;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args){
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        App app = new App();
+        app.run();
+//
+//        ArrayList<Object> todos = new ArrayList<>();
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.print("명령) ");
+//        String cmd = sc.nextLine();
+//
+//        if (cmd.equals("exit")){
+//            System.out.println("프로그램 종료");
+//            System.exit(0);
+//        } else if (cmd.equals("list")) {
+//            System.out.println("번호 / 내용");
+//
+//            for(Object todo1 : todos) {
+//                System.out.println(todo1);
+//            }
+//
+//        } else if (cmd.equals("add")) {
+//            System.out.print("할일 : ");
+//            String todo = sc.nextLine();
+//            todos.add(todo);
+//
+//            System.out.println("할 일이 생성되었습니다.");
+//        } else if (cmd.equals("del")) {
+//            System.out.println("삭제할 할 일의 번호 : ");
+//            String num = sc.nextLine();
+//
+//            todos.remove(num);
+//
+//        } else if (cmd.equals("modify")) {
+//            System.out.println("수정할 할 일의 번호 : ");
+//            String num = sc.nextLine();
+//
+//        }
+
+
     }
 }
