af0d4b4407a6090df25d0d3f010244ddbf0891e2
hrjo0120 <hrjo0120@gmail.com>
2024-11-04 14:54:59 +0900

    핵심기능 이전

--- src/main/java/com/koreait/Main.java
+++ src/main/java/com/koreait/Main.java
@@ -1,45 +1,8 @@
 package com.koreait;
 
 public class Main {
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
         App app = new App();
         app.run();
-//
-//        ArrayList<Object> todos = new ArrayList<>();
-//        Scanner sc = new Scanner(System.in);
-//
-//        System.out.print("명령) ");
-//        String cmd = sc.nextLine();
-//
-//        if (cmd.equals("exit")){
-//            System.out.println("프로그램 종료");
-//            System.exit(0);
-//        } else if (cmd.equals("list")) {
-//            System.out.println("번호 / 내용");
-//
-//            for(Object todo1 : todos) {
-//                System.out.println(todo1);
-//            }
-//
-//        } else if (cmd.equals("add")) {
-//            System.out.print("할일 : ");
-//            String todo = sc.nextLine();
-//            todos.add(todo);
-//
-//            System.out.println("할 일이 생성되었습니다.");
-//        } else if (cmd.equals("del")) {
-//            System.out.println("삭제할 할 일의 번호 : ");
-//            String num = sc.nextLine();
-//
-//            todos.remove(num);
-//
-//        } else if (cmd.equals("modify")) {
-//            System.out.println("수정할 할 일의 번호 : ");
-//            String num = sc.nextLine();
-//
-//        }
-
-
     }
 }
